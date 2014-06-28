394937a731e40e9d392c50e6d50822c338b6fdf1
Lin He <he9lin@gmail.com>
2014-06-28 11:44:58 -0700

    Just sync

--- src/kaggle_challenge/core.clj
+++ src/kaggle_challenge/core.clj
@@ -1,5 +1,21 @@
-(ns kaggle-challenge.core)
+(ns kaggle-challenge.core
+  (:use [cascalog.api]
+        [cascalog.checkpoint]
+        [cascalog.more-taps :only (hfs-delimited)])
+  (:require [clojure.string :as s]
+            [cascalog.logic [ops :as c] [vars :as v]]))
 
+(defn products_per_customer_date [input]
+  (<- [?product]
+      ))
+
+
+(defn trans []
+  (let [data-in "data/sample_transaction"
+        data-out "./output/total-sales-per-city/"]
+    (?- (hfs-delimited data-out :sinkmode :replace :delimiter ",")
+        (total-sales-per-city
+          (hfs-delimited data-in :delimiter ",")))))
 (defn foo
   "I don't do a whole lot."
   [x]
--- test/kaggle_challenge/core_test.clj
+++ test/kaggle_challenge/core_test.clj
@@ -2,6 +2,14 @@
   (:require [clojure.test :refer :all]
             [kaggle-challenge.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(fact "Query should return tuples of items frequently appear together"
+      (transactions_by_customer :path) => (produces [["richhickey" 2961]])
+      (provided
+        (complex-subquery :path) =>
+          [
+           ["9909-107143070-5072" "6901-103700030-16139"]
+           ["6901-103700030-16139" "6901-103700030-16139" "2119-101200010-10522"]
+           ["2210-103700030-5174" "2222-103700030-5122"]
+           ["2119-101200010-10522" "2628-103700030-2248"]
+           ["2628-103700030-2248"]
+          ]))
