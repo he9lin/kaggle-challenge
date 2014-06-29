994164e793b110e59637bd209a6dfbf9bd82edfe
Lin He <he9lin@gmail.com>
2014-06-28 21:16:49 -0700

    Change clojure ver

--- project.clj
+++ project.clj
@@ -5,13 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :aot [kaggle-challenge.core]
   :main kaggle-challenge.core
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [cascalog/cascalog-core "2.1.0"]
                  [cascalog/cascalog-checkpoint "2.1.0"]
                  [cascalog/cascalog-more-taps "2.1.0"]]
   :jvm-opts ["-Xms768m" "-Xmx768m"]
   :profiles {
+             :provided {:dependencies [[org.apache.hadoop/hadoop-core "1.1.2"]]}
              :dev  {:plugins [[lein-midje "3.1.1"]]
-                    :dependencies [[org.apache.hadoop/hadoop-core "1.1.2"]
-                                   [lein-midje "3.1.1"]
+                    :dependencies [[lein-midje "3.1.1"]
                                    [cascalog/midje-cascalog "2.1.0"]]}})
