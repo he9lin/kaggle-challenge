a5141a8f0cda43632d3741549c27ea99ac429565
Lin He <he9lin@gmail.com>
2014-07-06 16:03:24 -0700

    Up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kaggle-challenge "0.1.3-SNAPSHOT"
+(defproject kaggle-challenge "0.1.4-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
